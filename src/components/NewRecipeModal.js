96afd2363cfa1e75fc84f6576c495f04c3f03c90
nirch <nirh2@yahoo.com>
2020-01-09 13:44:55 +0200

    added form to new recipe modal + reading inputs

--- src/components/NewRecipeModal.js
+++ src/components/NewRecipeModal.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 class NewRecipeModal extends Component {
     constructor(props) {
         super(props);
-        
+
+        this.state = {
+            name: "",
+            desc: "",
+            img: ""
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.createRecipe = this.createRecipe.bind(this);
     }
-    
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    createRecipe() {
+        const { name, desc, img } = this.state;
+
+        alert(`${name} ${desc} ${img}`);
+    }
+
     render() {
         const { show, handleClose } = this.props;
+        const { name, desc, img } = this.state;
 
         return (
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>New Recipe</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control name="name" value={name}
+                                type="text" placeholder="Enter Recipe Name" onChange={this.handleInputChange} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control name="desc" value={desc}
+                                type="text" placeholder="Enter Recipe Description" onChange={this.handleInputChange} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Image URL</Form.Label>
+                            <Form.Control name="img" value={img}
+                                type="text" placeholder="Enter Recipe Image URL" onChange={this.handleInputChange} />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
-                        Close
+                        Cancel
                 </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                    <Button variant="success" onClick={this.createRecipe}>
+                        Create
                 </Button>
                 </Modal.Footer>
             </Modal>);
