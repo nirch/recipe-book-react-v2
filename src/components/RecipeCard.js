97af42b12e2d3d7e07b5cf073fa017b387f34d00
nirch <nirh2@yahoo.com>
2020-01-09 13:11:58 +0200

    done designing recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import './RecipeCard.css'
+import { Card } from 'react-bootstrap';
 
 class RecipeCard extends Component {
     constructor(props) {
         super(props);
-        
+
     }
-    
+
 
     render() {
         const { recipe } = this.props;
 
         return (
             <div className="recipe">
-                <p>{recipe.name}</p>
-                <img src={recipe.img}></img>
+                <Card>
+                    <Card.Img variant="top" src={recipe.img} />
+                    <Card.Body>
+                        <Card.Title>{recipe.name}</Card.Title>
+                        <Card.Text>
+                            {recipe.desc}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
         );
     }
