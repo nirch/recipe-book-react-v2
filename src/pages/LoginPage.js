9cf944d04e304e80eae1dd5ce94e3133c58b19cc
nirch <nirh2@yahoo.com>
2020-01-09 11:22:29 +0200

    reading inputs from login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,7 +4,35 @@ import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            pwd: ""
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    login() {
+        alert(this.state.email + " " + this.state.pwd);
+    }
+
     render() {
+        const { email, pwd } = this.state;
+
         return (
             <div className="p-login">
                 <div className="main">
@@ -13,14 +41,16 @@ class LoginPage extends Component {
                     <Form>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control name="email" value={email}
+                                type="email" placeholder="Enter email" onChange={this.handleInputChange} />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control name="pwd" value={pwd}
+                                type="password" placeholder="Password" onChange={this.handleInputChange} />
                         </Form.Group>
-                        <Button variant="success" type="button" block>
+                        <Button variant="success" type="button" block onClick={this.login}>
                             Login
                         </Button>
                     </Form>
