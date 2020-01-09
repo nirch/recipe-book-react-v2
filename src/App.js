152f8fe0c0dc5b6785021f5bdbc9138b42eeda4c
nirch <nirh2@yahoo.com>
2020-01-09 09:58:03 +0200

    react router + routes for home, login and recipes pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RecipesPage from './pages/RecipesPage';
+
 
 class App extends React.Component {
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return ( 
+      <Switch>
+        <Route exact path="/">
+          <HomePage/>
+        </Route>
+        <Route exact path="/login">
+          <LoginPage/>
+        </Route>
+        <Route exact path="/recipes">
+          <RecipesPage/>
+        </Route>
+
+      </Switch>
     );
   
   }
