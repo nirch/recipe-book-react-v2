0ac073fde12fae961ed2d725f2c71810263951e6
nirch <nirh2@yahoo.com>
2020-01-13 09:24:48 +0200

    Updates

---
+++ precache-manifest.e658376794c9d7d9f47158de66cefe8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e070710a33ddc5d9cb924b2fe33368ff",
+    "url": "/recipe-book-react-v2/index.html"
+  },
+  {
+    "revision": "28350b4ebcc4d7be514e",
+    "url": "/recipe-book-react-v2/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a73f69ad5322b5e0d5cb",
+    "url": "/recipe-book-react-v2/static/css/main.9479de29.chunk.css"
+  },
+  {
+    "revision": "28350b4ebcc4d7be514e",
+    "url": "/recipe-book-react-v2/static/js/2.35257429.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/recipe-book-react-v2/static/js/2.35257429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a73f69ad5322b5e0d5cb",
+    "url": "/recipe-book-react-v2/static/js/main.49d08084.chunk.js"
+  },
+  {
+    "revision": "2d332c69afe0a7c2ad12",
+    "url": "/recipe-book-react-v2/static/js/runtime-main.e5032292.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-book-react-v2/static/media/logo.5d5d9eef.svg"
+  }
+]);
